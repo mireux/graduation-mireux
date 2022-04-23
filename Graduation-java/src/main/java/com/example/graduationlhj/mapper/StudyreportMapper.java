3232c8fbcb5897bb15b0ae00c79acbcd0fe77596
mireux <799718728@qq.com>
2022-04-23 20:23:38 +0800

    增加学习记录

---
+++ Graduation-java/src/main/java/com/example/graduationlhj/mapper/StudyreportMapper.java
@@ -0,0 +1,16 @@
+package com.example.graduationlhj.mapper;
+
+import com.example.graduationlhj.entity.Studyreport;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author lhj
+ * @since 2022-04-23
+ */
+public interface StudyreportMapper extends BaseMapper<Studyreport> {
+
+}
