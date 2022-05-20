59717892074a452a7070da1d9989b04712c1925d
mireux <799718728@qq.com>
2022-05-20 22:35:14 +0800

    swagger注释添加 添加统计图表

--- graduation-web/src/views/admin/showInfo.vue
+++ graduation-web/src/views/admin/showInfo.vue
@@ -1,42 +1,129 @@
+<!--  在Echarts.vue文件中 -->
 <template>
-  <div class="showInfo">
-    <el-row>
-      <MixChart height="600px" width="100%"></MixChart>
-    </el-row>
+
+  <div class="Echarts">
+    <div id="main"></div>
   </div>
 </template>
 
 <script>
-import MixChart from '@/components/charts/MixChart'
-import LineMarker from '@/components/charts/LineMarker'
-import Keyboard from '@/components/charts/Keyboard'
+import * as echarts from 'echarts'
+import { getInfo, getRoom } from '@/api/showInfo'
 
 export default {
-  name: 'showInfo.vue',
-  components: { MixChart, LineMarker, Keyboard },
+  name: 'Monthly',
   data() {
-    return {}
+    return {
+      goodsName: '',
+      dataList: {chosenSeat:[],totalSeat:[]},
+      chosenSeat: [],
+      totalSeat: [],
+      roomList: [],
+      seriesList: []
+    }
   },
+  methods: {
+    myEcharts() {
+      var chartDom = document.getElementById('main')
+      var myChart = echarts.init(chartDom)
+      var option
 
-  computed: {},
-
-  watch: {},
-
-  methods: {},
-
-  created() {
+      option = {
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            // Use axis to trigger tooltip
+            type: 'shadow' // 'shadow' as default; can also be 'line' or 'shadow'
+          }
+        },
+        legend: {},
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: {
+          type: 'value'
+        },
+        yAxis: {
+          type: 'category',
+          data: this.roomList
+        },
+        series: this.seriesList
+      }
+      option && myChart.setOption(option)
+    },
+    getRoom() {
+      return new Promise((resolve, reject) => {
+        getRoom().then(resp => {
+          const { data } = resp
+          this.roomList = data
+          this.myEcharts()
+          console.log(this.roomList)
+        }).catch(error => {
+          reject(error)
+        })
+      })
+    },
+    // 获取所有的数据
+    getAllData() {
+      return new Promise((resolve, reject) => {
+        getInfo().then(resp => {
+          const { data } = resp
+          this.dataList = data
+          this.totalSeat = this.dataList.totalSeat
+          this.chosenSeat = this.dataList.chosenSeat
+          let series1 = {
+            name: '全部座位',
+            type: 'bar',
+            stack: 'total',
+            label: {
+              show: true
+            },
+            emphasis: {
+              focus: 'series'
+            },
+            data: this.totalSeat
+          }
+          let series2 = {
+            name: '已选座位',
+            type: 'bar',
+            stack: 'total',
+            label: {
+              show: true
+            },
+            emphasis: {
+              focus: 'series'
+            },
+            data: this.chosenSeat
+          }
+          this.seriesList.push(series1)
+          this.seriesList.push(series2)
+          this.myEcharts()
+          console.log(this.roomList)
+        }).catch(error => {
+          reject(error)
+        })
+      })
+    }
   },
-
   mounted() {
+    this.getAllData()
   },
-
-  updated() {
-  },
-
-  destroyed() {
+  created() {
+    this.getRoom()
   }
 }
 </script>
 
-<style scoped>
+<style>
+#main {
+  width: 1200px;
+  height: 500px;
+  margin-left: 5%;
+  margin-top: 20px;
+  /*background: aquamarine;*/
+}
 </style>
+
